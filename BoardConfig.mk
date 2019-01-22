7b5375c85c791364876e2e6dc7cda59762d52c2b
PIPIPIG233666 <2212848813@qq.com>
2019-01-23 07:03:49 +0800

    Revert "wayne:use clang to compile"
    This reverts commit 4247424381190470e3022f4adb5ee47a8827ff63.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,6 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/nvt_wake_gesture"
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm660
 TARGET_KERNEL_CONFIG := wayne-perf_defconfig
-TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := 8.0.7
 
 # Manifest
